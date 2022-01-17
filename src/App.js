3a7e53e7cfab6f6e1e68c3b80304b3a748dc9255
Julian Shaw <julianshaw2000@gmail.com>
2022-01-16 21:48:37 -0500

    changed app text

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>
-          Rastaman fix!
+          Rastaman fix on feature!
         </p>
         <a
           className="App-link"
